96fd3cbee78bd44b7c34745348566b9110d12aac
DanieleZ18 <52591625+DanieleZ18@users.noreply.github.com>
2026-03-30 15:06:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "App Runner Test: Status 200 OK"
+    return "ZEFH GAY"
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
